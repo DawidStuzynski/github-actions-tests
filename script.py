201b34b84f147492c3e21eaddb29c6c42bda7e62
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2024-03-14 20:58:00 +0100

    Add python workflow

--- script.py
+++ script.py
@@ -1,1 +1,15 @@
-print("Hello")
+import os
+
+# Create a folder if it doesn't exist
+folder_name = 'artifact-folder'
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
+
+# Specify the path to the file inside the folder
+file_path = os.path.join(folder_name, 'example.txt')
+
+# Open a file in write mode ('w') inside the folder
+with open(file_path, 'w') as file:
+    file.write('Hello, this is a sample text.\n')
+    file.write('This is another line of text.\n')
+    file.write('And one more line for good measure.\n')
